982d42a3959eb5bac0d37b76d8a6577d72e63694
vova371 <vova371@gmail.com>
2020-01-27 14:42:45 +0000

    improved page parsing and error handling

--- knigavuhe.py
+++ knigavuhe.py
@@ -10,8 +10,11 @@ from urllib.parse import urlparse
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='knigavuhe')
-parser.add_argument("--book", type=str, help='book url')
-parser.add_argument("--path", type=str, default='.', help='output directory')
+parser.add_argument('-b', '--book', type=str, help='book url')
+parser.add_argument('-p', '--path', type=str, default='.', help='output directory')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+group.add_argument('-q', '--quiet', action='store_true', help='suppress console output')
 args = parser.parse_args()
 
 directory = args.path
@@ -22,17 +25,25 @@ if not os.path.exists(directory):
 with urlopen(args.book) as response:
     page = response.read().decode('utf-8')
 
-match = re.search(r"(BookPlayer)[^;]*(\[.*audio[^\]]*\])", page)
+match = re.search(r'(BookPlayer)\w*\((.*audio.*)\)\w*;', page)
 
 if not match:
-    print('failed to find book chapters')
+    if not args.quiet:
+        print('failed to find book player audio files')
+    sys.exit(1)
 
-chapters = json.loads(match.group(2))
+if args.verbose:
+    print('book player args: {}'.format(match.group(2)))
 
-for chapter in chapters:
+book_player_args = json.loads('[' + match.group(2) + ']')
+
+for chapter in book_player_args[1]:
     url = chapter['url']
     name = os.path.basename(urlparse(url).path)
     path = os.path.join(directory, name)
-    print('{} -> {}'.format(url, path))
+
+    if not args.quiet:
+        print('{} -> {}'.format(url, path))
+
     with urlopen(url) as response, open(path, 'wb') as output:
         output.write(response.read())
